70256dfe4992cdb35d799e2138b2a21d5bf6d936
coreybb <corey@lakonic.dev>
2024-10-16 10:15:03 -0400

    Initial Commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Corey Beebe on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
